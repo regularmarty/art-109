5587c0ccdb9a8b9430d204fde415a4dbb4b32324
regularmarty <regularmartin@hotmail.com>
2026-03-21 02:49:17 -0700

    commit

---
+++ tutorials/2-css/06-dom/js/main.js
@@ -0,0 +1,56 @@
+// console.log("im here")
+
+
+// select html elements
+const header = document.querySelector("#header");
+const changeHeaderButton = document.querySelector("#change-header-button");
+const changeThemeButton= document.querySelector("#change-theme-button");
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+
+const img4 = document.querySelector("#img4");
+
+
+//change header with buttom click
+changeHeaderButton.addEventListener("click",()=> {
+    header.innerHTML = "POW!!!"
+})
+
+
+
+//toggle color theme 
+
+//create function for changing button text
+function changeButtonText(){
+    if(document.body.classList.contains("dark")){
+        changeThemeButton.textContent = "Switch to Light Theme";
+    } else {
+        changeThemeButton.textContent = "Switch to Dark Theme";
+    } 
+}
+
+
+// click event on button
+
+changeThemeButton.addEventListener("click", ()=> {
+    //add/remove dark class to body
+    document.body.classList.toggle("dark");
+    changeButtonText();
+
+})
+
+
+//toggle image visibility 
+
+img1.addEventListener("click", () => {
+    img2.classList.remove("hidden");
+
+})
+
+
+img2.addEventListener("click", () => {
+
+    img4.classList.remove("hidden");
+})
+
+
