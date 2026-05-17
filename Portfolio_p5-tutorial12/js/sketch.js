ba0433c57bbfcff539981e8f10cab68952e4970f
regularmarty <regularmartin@hotmail.com>
2026-05-17 12:53:15 -0700

    submit

--- Portfolio_p5-tutorial12/js/sketch.js
+++ Portfolio_p5-tutorial12/js/sketch.js
@@ -22,7 +22,12 @@ function draw(){
 }
 
 function mouseMoved(){
+   drawThing(mouseX,mouseY);
+   drawThing(mouseX - 50, mouseY + 75);
+}
+
+function drawThing(_x,_y){
     strokeWeight(0);
     fill(random(200,255),random(200,255),random(200,255));
-    ellipse(mouseX, mouseY, 30, 30);
+    ellipse(_x, _, 30, 30);
 }
